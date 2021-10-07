23a16eee36a692e2deaf7ff11b2f433631162c2d
Anastasia <aabogat92@mail.ru>
2021-10-07 21:06:17 +0300

    функция генерации объектов

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function returnAnyNumber (from, to) {
+/*function returnAnyNumber (from, to) {
   if (from >= 0 && to > 0 && from < to) {
     return Math.floor(Math.random() * (to - from + 1)) + from;
   }
@@ -20,4 +20,120 @@ function checkStringLength (string, maxLength) {
   return false;
 }
 
-checkStringLength ('Напишите комментарий', 140);
+checkStringLength ('Напишите комментарий', 140);*/
+
+
+const DESCRIPTIONS = [
+  'Очень красивое фото',
+  'Фото заката',
+  'Селфи',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'José',
+  'María',
+  'Antonio',
+  'Carmen',
+  'Juan',
+  'Ana',
+  'Manuel',
+  'Isabel',
+  'Francisco',
+  'Dolores',
+];
+
+const arrayId = [];
+const arrayUrl = [];
+const arrayCommentsId = [];
+
+const createFoto = () => {
+  //id
+  const randomId = function(lower, upper) {
+    let id = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+    while (arrayId.includes(id)) {
+      continue;
+    }
+    arrayId.push(id);
+    return id;
+  };
+
+  //url
+  const randomUrl = function(lower, upper) {
+    let url = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+    while (arrayUrl.includes(url)) {
+      continue;
+    }
+    arrayUrl.push(url);
+    return `photos/${  url  }.jpg`;
+  };
+
+  //описание
+  const description = function() {
+    let randomDescription = Math.floor(Math.random() * DESCRIPTIONS.length);
+    return DESCRIPTIONS[randomDescription];
+  };
+
+  //лайки
+  const likes = function(min, max) {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result;
+  };
+
+  //комментарии
+  const createComments = () => {
+    //комментарии-id
+    const randomCommentsId = function() {
+      for (let i = 0; i < arrayCommentsId.length + 1; i++) {
+        let commentsId = arrayCommentsId.length + 1;
+        arrayCommentsId.push(commentsId);
+        return arrayCommentsId.length;
+      }
+    };
+
+    //аватарки
+    const avatar = function(min, max) {
+      const result = Math.floor(Math.random() * (max - min + 1)) + min;
+      return `img/avatar-${  result  }.svg`;
+    };
+
+    //текст комментария
+    const textComments = function() {
+      let randomMessage = Math.floor(Math.random() * MESSAGES.length);
+      return MESSAGES[randomMessage];
+    };
+
+    //имя комментатора
+    const nameOfCommentator = function() {
+      let randomName = Math.floor(Math.random() * NAMES.length);
+      return NAMES[randomName];
+    };
+
+    return {
+      id: randomCommentsId(),
+      avatar: `${avatar(1, 6)  }`,
+      message: `${textComments(MESSAGES)  }`,
+      name: `${nameOfCommentator(NAMES)  }`,
+    };
+  };
+
+  return {
+    id: randomId(1, 25),
+    url: `${randomUrl(1, 25)  }`,
+    description: `${description(DESCRIPTIONS)  }`,
+    likes: likes(15, 200),
+    comments: createComments(),
+  };
+};
+
+const arrayOfCreateFoto = Array.from({length: 3}, createFoto);
+
+console.log(arrayOfCreateFoto);
