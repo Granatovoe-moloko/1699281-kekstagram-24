eb6ec79ee63b0033eee5220867236fd0ecf5ebff
Anastasia <aabogat92@mail.ru>
2021-11-03 13:07:48 +0300

    внесла исправления + сделала закрытие

--- js/full-photo.js
+++ js/full-photo.js
@@ -1,42 +1,32 @@
 const fullPhoto = document.querySelector('.big-picture');
 const fullPhotoImg = fullPhoto.querySelector('.big-picture__img img');
 const fullPhotoComments = fullPhoto.querySelector('.social__comments');
+const fullPhotoComment = fullPhoto.querySelector('.social__comment');
 
 const showFullPhotoComments = (photoComments) => {
   const commentFragment = document.createDocumentFragment();
 
   photoComments.forEach((comment) => {
     const {avatar, name, message} = comment;
+    const commentpattern = fullPhotoComment.cloneNode(true);
 
-    const commentItem = document.createElement('li');
-    commentItem.classList.add('social__comment');
+    commentpattern.querySelector('.social__picture').src = avatar;
+    commentpattern.querySelector('.social__picture').alt = name;
+    commentpattern.querySelector('.social__text').textContent = message;
 
-    const commentator = document.createElement('img');
-    commentator.classList.add('social__picture');
-    commentator.src = avatar;
-    commentator.alt = name;
-    commentator.style.width = '35';
-    commentator.style.height = '35';
-    commentItem.appendChild(commentator);
-
-    const commentText = document.createElement('p');
-    commentText.classList.add('social__text');
-    commentText.textContent = message;
-    commentItem.appendChild(commentText);
-
-    commentFragment.appendChild(commentItem);
+    commentFragment.appendChild(commentpattern);
   });
   fullPhotoComments.innerHTML = '';
   fullPhotoComments.appendChild(commentFragment);
 };
 
-const showFullPhoto = (pictureData, photoComments) => {
+const showFullPhoto = (pictureData) => {
   const {url, likes, comments, description} = pictureData;
   fullPhoto.classList.remove('hidden');
   fullPhotoImg.src = url;
   fullPhoto.querySelector('.likes-count').textContent = likes;
   fullPhoto.querySelector('.comments-count').textContent = comments.length;
-  showFullPhotoComments(photoComments);
+  showFullPhotoComments(comments);
   fullPhoto.querySelector('.social__caption').textContent = description;
 };
 
--- js/previews.js
+++ js/previews.js
@@ -1,11 +1,12 @@
 import {showFullPhoto} from './full-photo.js';
-import {commentsCount} from './mock/create-photo-data.js';
 
 const previewList = document.querySelector('.pictures');
 const previewPattern = document.querySelector('#picture').content.querySelector('.picture');
 const commentCounter = document.querySelector('.social__comment-count');
 const commentLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
+const fullPhoto = document.querySelector('.big-picture');
+const fullPhotoClose = document.querySelector('.big-picture__cancel');
 
 
 const showPreviews = (picturesData) => {
@@ -18,14 +19,32 @@ const showPreviews = (picturesData) => {
     preview.querySelector('.picture__comments').textContent = comments.length;
     fragment.appendChild(preview);
 
-    preview.addEventListener('click', (evt) => {
+    function openPhoto (evt) {
       evt.preventDefault();
-      const photoComments = commentsCount(2);
-      showFullPhoto(pictureData, photoComments);
+      showFullPhoto(pictureData);
       commentCounter.classList.add('hidden');
       commentLoader.classList.add('hidden');
       body.classList.add('modal-open');
+    }
+
+    preview.addEventListener('click', openPhoto);
+    //fullPhotoClose.removeEventListener('click', openPhoto);
+    fullPhotoClose.addEventListener('click', () => {
+      fullPhoto.classList.add('hidden');
+      commentCounter.classList.remove('hidden');
+      commentLoader.classList.remove('hidden');
+      body.classList.remove('modal-open');
     });
+
+    document.addEventListener('keydown', (evt) => {
+      if (evt.keyCode === 27) {
+        fullPhoto.classList.add('hidden');
+        commentCounter.classList.remove('hidden');
+        commentLoader.classList.remove('hidden');
+        body.classList.remove('modal-open');
+      }
+    });
+
   });
   previewList.appendChild(fragment);
 };
